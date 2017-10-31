1b3cb98a201711c1c269cdbadea587713c18f9ac
dofris <lucasgranjard@hotmail.fr>
2017-10-31 13:07:26 +0100

    jeu.php

--- safariz/jeu.php
+++ safariz/jeu.php
@@ -2,5 +2,41 @@
 
 class jeu
 {
+    private  $datedebut;
+    private  $datefin;
+    /**
+     * @return the $datedebut
+     */
+    public function getDatedebut()
+    {
+        return $this->datedebut;
+    }
+
+    /**
+     * @return the $datefin
+     */
+    public function getDatefin()
+    {
+        return $this->datefin;
+    }
+
+    /**
+     * @param field_type $datedebut
+     */
+    public function setDatedebut($datedebut)
+    {
+        $this->datedebut = $datedebut;
+    }
+
+    /**
+     * @param field_type $datefin
+     */
+    public function setDatefin($datefin)
+    {
+        $this->datefin = $datefin;
+    }
+
+    
+    
 }
 
