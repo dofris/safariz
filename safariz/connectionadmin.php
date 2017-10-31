74c208ca7b72a85e9726a5328507c3180247ee48
dofris <lucasgranjard@hotmail.fr>
2017-10-31 09:17:40 +0100

    connexion admin commencé

---
+++ safariz/connectionadmin.php
@@ -0,0 +1,52 @@
+<?php
+
+class connectionadmin
+{
+    
+    
+    private $username;
+    private $mdp;
+    
+   function  __construct($user, $pd){
+       $this->mdp=$pd;
+       $this->username=$user;
+        
+    }
+    /**
+     * @return the $username
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return the $mdp
+     */
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
+
+    /**
+     * @param field_type $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @param field_type $mdp
+     */
+    public function setMdp($mdp)
+    {
+        $this->mdp = $mdp;
+    }
+
+    
+    
+    
+    
+}
+
